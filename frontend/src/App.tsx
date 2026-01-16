412385e59b868a1db0784c4d1f124bfe51e76b85
Antti Alestalo <alestalo.antti@gmail.com>
2026-01-16 20:26:39 +0200

    add table to display daily statistics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,28 +4,45 @@ import { useQuery } from '@tanstack/react-query';
 function App() {
   const { isPending, error, data } = useQuery({
     queryKey: ['dailyStatistics'],
-    queryFn: () =>
-      fetch('http://localhost:3000/api/dailystatistics').then((res) =>
-        res.json(),
-      ),
-  })
+    queryFn: () => fetch('http://localhost:3000/api/dailystatistics').then((res) => res.json()),
+  });
 
-  if (isPending) return 'Loading...'
+  if (isPending) return 'Loading...';
 
-  if (error) return 'An error has occurred: ' + error.message
+  if (error) return 'An error has occurred: ' + error.message;
+
+  //Response model
+  interface DailyStatistics {
+    date: string | null;
+    total_production_amount: number | null;
+    total_consumption_amount: number | null;
+    avg_hourly_price: number | null;
+    max_negative_price_streak_hours: number | null;
+  }
+
+  const stats: DailyStatistics[] = data;
 
   return (
     <div>
       <h1>Electricity Daily Statistics</h1>
-      <p>
-        {data[0].date}{' '}
-        {data[0].avg_production_amount}{' '}
-        {data[0].avg_consumption_amount}{' '}
-        {data[0].avg_hourly_price}{' '}
-        {data[0].max_negative_price_streak_hours}{' '}
-      </p>
+      <table>
+        <tr key={'header'}>
+          <th>Date</th>
+          <th>Total consumption</th>
+          <th>Total production</th>
+          <th>Average hourly price</th>
+          <th>Longest consecutive period when price was negative (hours)</th>
+        </tr>
+        {stats.map((item) => (
+          <tr key={item.date}>
+            {Object.values(item).map((val) => (
+              <td>{val}</td>
+            ))}
+          </tr>
+        ))}
+      </table>
     </div>
-  )
-};
+  );
+}
 
 export default App;
