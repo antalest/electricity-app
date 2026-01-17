f763b03880b0b34d1d1ce2f11b31d8999a1e77b7
Antti Alestalo <alestalo.antti@gmail.com>
2026-01-17 23:13:11 +0200

    refactor to have column labels and keys in same array

--- frontend/src/components/DailyStatisticsTable.tsx
+++ frontend/src/components/DailyStatisticsTable.tsx
@@ -10,41 +10,38 @@ interface DailyStatistics {
 
 type DailyStatisticsArray = DailyStatistics[];
 
-const columns: Array<string> = [
-  "Date",
-  "Total production amount",
-  "Total consumption amount",
-  "Avg hourly price",
-  "Max negative price streak in hours"
+interface Column {
+  label: string;
+  key: keyof DailyStatistics;
+}
+
+const columns: Array<Column> = [
+  {label: "Date", key: "date"},
+  {label: "Total production amount", key: "total_production_amount"},
+  {label: "Total consumption amount", key: "total_consumption_amount"},
+  {label: "Avg hourly price", key: "avg_hourly_price"},
+  {label: "Max negative price streak in hours", key: "max_negative_price_streak_hours"},
 ];
 
-const entrykeys: Array<keyof DailyStatistics> = [
-  "date",
-  "total_production_amount",
-  "total_consumption_amount",
-  "avg_hourly_price",
-  "max_negative_price_streak_hours"
-]
-
-const Header = (props: {columns: Array<string>}) => {
+const Header = (props: {columns: Array<Column>}) => {
   return (
     <thead>
       <tr>
         {props.columns.map((column) => (
-          <th key={column}>{column}</th>
+          <th key={column.key}>{column.label}</th>
         ))}
       </tr>
     </thead>
   );
 };
 
-const Content = (props: {entries: DailyStatisticsArray, entrykeys: Array<keyof DailyStatistics>}) => {
+const Content = (props: {entries: DailyStatisticsArray, columns: Array<Column>}) => {
   return (
     <tbody>
       {props.entries.map((entry) => (
         <tr key={entry.date}>
-          {entrykeys.map((entrykey) => (
-            <td key={entrykey}>{entry[entrykey]}</td>
+          {columns.map((column) => (
+            <td key={column.key}>{entry[column.key]}</td>
           ))}
         </tr>
       ))}
@@ -70,7 +67,7 @@ const DailyStatisticsTable = () => {
     <div>
       <table>
         <Header columns={columns}/>
-        <Content entries={data} entrykeys={entrykeys}/>
+        <Content entries={data} columns={columns}/>
       </table>
     </div>
   );
